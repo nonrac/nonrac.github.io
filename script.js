5f89d6b100e8788ea3f66735c8081eaec69dd3ad
nonrac <117037274+nonrac@users.noreply.github.com>
2024-01-25 11:56:46 -0600

    l

--- script.js
+++
@@ -1,9 +0,0 @@
-// Add this to the end of your script.js file
-document.getElementById('logout-link').addEventListener('click', async () => {
-    try {
-        await fetch('/logout', { method: 'POST' });
-        window.location.reload();
-    } catch (error) {
-        console.error('Error during logout:', error);
-    }
-});

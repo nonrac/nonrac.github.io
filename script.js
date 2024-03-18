a2d68d9d01a81790f68079166f5a1d37dada4978
nonrac <117037274+nonrac@users.noreply.github.com>
2024-03-18 18:09:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function showCars() {
   document.getElementById('carImage').classList.remove('hidden');
   document.getElementById('truckImage').classList.add('hidden');
+  document.getElementById('searchInput').classList.add('hidden');
 }
 
 function showTrucks() {
   document.getElementById('carImage').classList.add('hidden');
   document.getElementById('truckImage').classList.remove('hidden');
+  document.getElementById('searchInput').classList.remove('hidden');
 }
 
 function search() {
   var year = document.getElementById('searchInput').value;
-  // Your search logic here, maybe filter based on the year
+  // Your search logic here, maybe trigger a search using an API
 }

96e55f79fdd93ce73741b8571de37f07693259e9
nonrac <117037274+nonrac@users.noreply.github.com>
2024-04-11 22:02:24 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function packCode() {
-    var inputCode = document.getElementById('codeInput').value;
-    // Send the inputCode to the backend for packing
-    // Receive the packed code from the backend
-    // For demonstration, I'll just alert the packed code
-    alert("Packed code: " + inputCode);
-}

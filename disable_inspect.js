de4fde8f8a4ec0a5f303b0aad4124939bc416974
nonrac <117037274+nonrac@users.noreply.github.com>
2024-05-25 13:34:21 -0400

    Add files via upload

---
+++ disable_inspect.js
@@ -0,0 +1,17 @@
+document.addEventListener('contextmenu', (_0x5ac235) =>
+    _0x5ac235.preventDefault()
+  )
+  document.onkeydown = function (_0x20e248) {
+    if (_0x20e248.keyCode == 123) {
+      return false
+    }
+    if (_0x20e248.ctrlKey && _0x20e248.shiftKey && _0x20e248.keyCode == 73) {
+      return false
+    }
+    if (_0x20e248.ctrlKey && _0x20e248.shiftKey && _0x20e248.keyCode == 74) {
+      return false
+    }
+    if (_0x20e248.ctrlKey && _0x20e248.keyCode == 85) {
+      return false
+    }
+  }
